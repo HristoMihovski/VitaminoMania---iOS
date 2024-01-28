0d9e2326c47a578720f8117f155ff6a75b48fbae
HristoMihovski <hristo.mihovski.highschool@svetlina.net>
2024-01-28 13:38:09 +0200

    Initial Commit

---
+++ VitaminoMania/App/Spring.swift
@@ -0,0 +1,31 @@
+import Foundation
+import UIKit
+
+class Spring: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let array = ["Monkey", "Tiger"]
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        myTableView.dataSource = self
+        myTableView.delegate = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Spring", for: indexPath)
+        
+        cell.textLabel!.text = array[indexPath.row]
+        
+        return cell
+    }
+}

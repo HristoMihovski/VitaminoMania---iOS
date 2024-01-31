5563bf124ce3892121be478789244daba75fa7de
HristoMihovski <hristo.mihovski.highschool@svetlina.net>
2024-01-31 16:37:41 +0200

    Added Firebase Storage and Firestore. Fixed the UITable problem

--- VitaminoMania/App/Spring.swift
+++ VitaminoMania/App/Spring.swift
@@ -1,31 +1,65 @@
-import Foundation
 import UIKit
+import Firebase
 
 class Spring: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    let array = ["Monkey", "Tiger"]
+
+    let array = ["Apple", "Pear"]
+    var firestore: Firestore!
     
-    @IBOutlet weak var myTableView: UITableView!
-    
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        myTableView.dataSource = self
-        myTableView.delegate = self
+        
+        // Initialize Firestore
+        firestore = Firestore.firestore()
+        
+        // Set up table view
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-    
+
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return array.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Spring", for: indexPath)
-        
+
         cell.textLabel!.text = array[indexPath.row]
-        
+
         return cell
+    }
+
+    // Function to store data in Firestore
+    func storeDataInFirestore(text: String) {
+        firestore.collection("yourCollectionName").addDocument(data: [
+            "text": text
+        ]) { error in
+            if let error = error {
+                print("Error adding document: \(error)")
+            } else {
+                print("Document added successfully")
+            }
+        }
+    }
+
+    // Function to retrieve data from Firestore
+    func retrieveDataFromFirestore() {
+        firestore.collection("yourCollectionName").getDocuments { (querySnapshot, error) in
+            if let error = error {
+                print("Error getting documents: \(error)")
+            } else {
+                for document in querySnapshot!.documents {
+                    let text = document.data()["text"] as? String ?? ""
+                    print("Text from Firestore: \(text)")
+                    // You can use the retrieved text as needed
+                }
+            }
+        }
     }
 }
